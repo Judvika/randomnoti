e98a335edcf844b29e3a399f4bcf1331003da164
co-20020455 <hyunho.kang@coway.com>
2023-02-09 16:40:40 +0900

    2. dialog 까지 완료

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,61 @@ import 'package:flutter/material.dart';
 import 'notification.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+      MaterialApp(
+          home: MyApp()
+      )
+  );
 }
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    return Scaffold(
         appBar: AppBar(title: Text('hello'),),
         body: Text('hello body'),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            showDialog(context: context, builder: (context){
+              return addItemDialog();
+            });
+          },
+          backgroundColor: Colors.green,
+          child: Icon(Icons.add),
+        ),
         bottomNavigationBar: BottomAppBar(
           child: Text('hello bottom'),
+        ),
+
+    );
+  }
+}
+class addItemDialog extends StatelessWidget {
+  const addItemDialog({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: Container(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text("TO-DO"),
+                Expanded(child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Hint',
+                    )
+                ))
+
+              ],
+            )
+          ],
         ),
       ),
     );
   }
 }
+
